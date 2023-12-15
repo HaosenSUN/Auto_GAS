9d908de4a57a0dfe37f8f2ae02a3ec8f23379452
SUN Haosen <103928629+HaosenSUN@users.noreply.github.com>
2023-12-15 11:10:13 +0800

    Add files via upload

---
+++ gaswot/log_utils/__init__.py
@@ -0,0 +1,3 @@
+from .logger import *  # noqa: F401, F403
+from .meters import *  # noqa: F401, F403
+from .model_db import *  # noqa: F401, F403
---
+++ gaswot/log_utils/logger.py
@@ -0,0 +1,18 @@
+import time
+
+
+class Logger(object):
+
+    def __init__(self, path, mode='w+'):
+        self.log_file = open(path, mode)
+        self.write('logger initiated!')
+        self.write(f'logger_path {path}')
+
+    def write(self, message):
+        self.log_file.write(f"[{time.strftime('%Y-%m-%d %H:%M')}] " +
+                            str(message) + '\n')
+        print(f"[{time.strftime('%Y-%m-%d %H:%M')}] " + str(message))
+        self.log_file.flush()
+
+    def close(self):
+        self.log_file.close()
